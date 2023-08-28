3f83d9864475e7f087ce2455abe61322327d59ab
otmorales23 <oliverwills0227@gmail.com>
2023-08-28 16:44:05 -0700

    connected to sequelize and set up models

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
+// 1. connect using sequelize
+
+// 2. setup models
+
+// 3. setup associations
+
+// 4. work on routes
+
